778379f410a5ce558c51d77756b6b864e8c41062
nervous <linkbleu2000@hotmail.com>
2011-10-05 23:20:17 +0200

    ajout de dossiers dans l'arborescence du projet + init particules (commenter tout ce qui concerne les particules si vous voulez compiler)
    Signed-off-by: nervous <linkbleu2000@hotmail.com>

---
+++ Xspace/Xspace/Particules/Particule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Xspace
+{
+    class Particule
+    {
+        private readonly ParticuleOptions _options;
+        private readonly ParticuleMgr _mgr;
+
+        public bool Active; // etat de la particule, active ou non
+
+        public Vector2 Position { get; set; } // vecteur de position courante de la particule
+        public double ActifTime { get; set; } // temps d'activité restante de la particule
+        public Vector2 Vitesse { get; set; }
+    }
+}
---
+++ Xspace/Xspace/Particules/ParticulesOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xspace
+{
+   public class ParticulesOptions
+    {
+       public struct ParticulesOptions 
+       {
+           public Couleur CouleurInit {get; set;} // couleur des particules
+           
+           public int ParticulesMax {get; set;} // nombre max de particules
+           public double ActifTime {get; set;} 
+
+           public ParticulesOptions(
+
+    }
+}
