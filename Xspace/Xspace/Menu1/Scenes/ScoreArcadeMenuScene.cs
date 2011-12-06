2cbbe74c9efd2fa2c6314e7e29f456db02d714ff
nervous <linkbleu2000@hotmail.com>
2011-12-06 23:21:03 +0100

    Ajout du nouveau fond score + delete debug  / suo

--- Xspace/Xspace/Menu1/Scenes/ScoreArcadeMenuScene.cs
+++ Xspace/Xspace/Menu1/Scenes/ScoreArcadeMenuScene.cs
@@ -13,13 +13,12 @@ namespace MenuSample.Scenes
 {
     public class ScoreArcadeMenuScene : AbstractMenuScene
     {
-        private readonly MenuItem _scoreMenuItem;
         private ContentManager _content;
         private SpriteFont _gamefont;
         private Texture2D _score_board;
         private StreamReader sr;
         private string path;
-        private Vector2 position_score1;
+        private Vector2 position_score1, position_Nv1;
         private Vector2 position_board;
         public string[] score;
         private string stock;
@@ -28,29 +27,28 @@ namespace MenuSample.Scenes
             : base(sceneMgr, "Score Arcade")
         {
             path = "Arcade.txt";
-            var back = new MenuItem("Retour");
+           var back = new MenuItem("Retour");
+            var Nv1 = new MenuItem("Nv.1");
             sr = new StreamReader(path);
-
+            position_Nv1.X = 130;
+            position_Nv1.Y = 195;
+            position_score1.X = 210;
+            position_score1.Y = 195;
+            position_board.X = 0;
+            position_board.Y = 0;
 
             back.Selected += OnCancel;
+
+            MenuItems.Add(back); 
             
-            MenuItems.Add(back);
+        
         }
 
-        public override void Update(GameTime gameTime)
-        {
-
-        }
 
         public override void Draw(GameTime gameTime)
         {
-
             stock = sr.ReadToEnd();
             score = System.IO.File.ReadAllLines(@path);
-            position_score1.X = 415;
-            position_score1.Y = 170;
-            position_board.X = 0;
-            position_board.Y = 0;
 
 
           if(_content == null)
@@ -63,7 +61,8 @@ namespace MenuSample.Scenes
             spriteBatch.Begin();
             
                 spriteBatch.Draw(_score_board, position_board, Color.White);
-                spriteBatch.DrawString(_gamefont, score[0], position_score1, Color.Green);
+                spriteBatch.DrawString(_gamefont, score[0], position_score1, Color.Green, 0, position_board, 0.7f, SpriteEffects.None, 0);
+                spriteBatch.DrawString(_gamefont, "Nv1", position_Nv1, Color.Green, 0, position_board, 0.7f, SpriteEffects.None, 0);
             spriteBatch.End();
         }
 
