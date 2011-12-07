08d71550cde94a0da3104d88da70ee7610644e70
unknown <Nervous@.(none)>
2011-12-07 14:18:49 +0100

    Ajout du cadre de surbrillance pour selectionner les scores

--- Xspace/Xspace/GameplayScene.cs
+++ Xspace/Xspace/GameplayScene.cs
@@ -117,7 +117,7 @@ namespace MenuSample.Scenes
             _gameFont = _content.Load<SpriteFont>("Fonts\\Menu\\Menu");
 
             particleRenderer.LoadContent(_content);
-            particleEffect = _content.Load<ParticleEffect>(("Collisions\\BasicExplosion\\BasicExplosion"));
+            particleEffect = _content.Load<ParticleEffect>("Collisions\\BasicExplosion\\BasicExplosion");
             particleEffect.LoadContent(_content);
             particleEffect.Initialise();
 
