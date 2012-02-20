7858598f95c53445e04edb2fca08a3426152647f
Antoine Pietri <antoine.pietri@epita.fr>
2012-02-20 23:21:16 +0100

    Fix des coeffs du background

--- Xspace/Xspace/GameplayScene.cs
+++ Xspace/Xspace/GameplayScene.cs
@@ -330,7 +330,7 @@ namespace MenuSample.Scenes
             float coeff_speed_variation = 1f; //coefficient de la variation de la vitesse des fonds.
             float coeff_speed = 0.05f; //coefficient de vitesse du fond.
             float coeff_speed_middle = 0.1f; //coefficient de vitesse du fond au milieu.
-            float coeff_speed_front = 0.5f; //coefficient de vitesse du fond en avant.
+            float coeff_speed_front = 0.8f; //coefficient de vitesse du fond en avant.
             float default_speed = 2f; //La vitesse doit tendre vers (default_speed * coeff)
 
             fond_ecran.Update(fps_fix, (default_speed + (music_energy - default_speed) * coeff_speed_variation) * coeff_speed);
@@ -339,7 +339,7 @@ namespace MenuSample.Scenes
 
             AudioPlayer.Update();
 
-            #region Gestion de la musique en cas de pause   A REACTIVER APRES PREMIERE SOUTENANCE 
+            #region Gestion de la musique en cas de pause
             if (InputState.IsPauseGame())
             {
                 AudioPlayer.SetVolume(0.2f);
