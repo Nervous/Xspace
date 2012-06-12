4776a20a364f3246682df85b0d56d3d508856302
Antoine Vallee <linkbleu2000@hotmail.com>
2012-06-12 13:59:08 +0200

    Push du leveleditor terminé (il reste plus que les finitions !)
    Signed-off-by: Antoine Vallee <linkbleu2000@hotmail.com>

---
+++ LevelEditor/LevelEditor/LevelEditor/LevelEditor.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LevelEditor
+{
+    public partial class LevelEditor : Form
+    {
+        bool draw_all_black;
+        Image saveimage;
+        int selected, page, ligne;
+        bool hasClicked, reset;
+        string path = "level.xpa";
+        protected Stream stream;
+        List<int> emplacements = new List<int>();
+
+
+        public LevelEditor()
+        {
+            draw_all_black = true;
+            selected = 0;
+            page = 0;
+            ligne = 0;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e) //drone 1
+        {
+            
+            if ((selected!=1)&&(!reset)) // Clic de base pour séléctionner le vaisseau
+            {
+                saveimage = pictureBox4.Image;
+                resetPictures(sender, e, 1);
+                pictureBox4.Image = global::LevelEditor.Properties.Resources.dronebrillance;
+                selected = 1;             
+            }
+            else if (!reset) // On retire le rectangle rouge quand le joueur reclique 
+            {
+                pictureBox4.Image = global::LevelEditor.Properties.Resources.drone;
+                selected = 0;
+                saveimage = global::LevelEditor.Properties.Resources.empty;
+            }
+            else if (selected == 1)
+            {// En cas de reset (suppression du rectangle)
+                pictureBox4.Image = global::LevelEditor.Properties.Resources.drone;
+            }
+        }
+
+        private void resetPictures(object sender, EventArgs e, int avoid)
+        {
+            reset = true;
+            if(avoid != 1)
+            pictureBox4_Click(sender, e); 
+
+            if (avoid != 2)
+            pictureBox3_Click(sender, e);
+
+            if(avoid != 3)
+                pictureBox1_Click(sender, e);
+
+            if(avoid != 4)
+                pictureBox2_Click(sender, e);
+
+            if(avoid != 5)
+                pictureBox6_Click_1(sender, e);
+
+            reset = false;
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if ((selected != 2)&&(!reset))
+            {
+                resetPictures(sender, e, 2);
+                saveimage = pictureBox3.Image;
+                pictureBox3.Image = global::LevelEditor.Properties.Resources.doubleshooterbrillance;
+                selected = 2;
+            }
+            else if (!reset)
+            {
+                pictureBox3.Image = global::LevelEditor.Properties.Resources.doubleshooter;
+                selected = 0;
+                saveimage = global::LevelEditor.Properties.Resources.empty;
+            }
+            else if (selected == 2)
+            {
+                pictureBox3.Image = global::LevelEditor.Properties.Resources.doubleshooter;
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (MessageBox.Show("Quitter l'éditeur de niveaux ?",
+                     "Message de confirmation",
+                         MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();   
+        }
+
+        private void importerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Import");
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            pictureBox7.Image = saveimage;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if ((selected != 3) && (!reset))
+            {
+                resetPictures(sender, e, 3);
+                saveimage = pictureBox1.Image;
+                pictureBox1.Image = global::LevelEditor.Properties.Resources.energizerbrillanc;
+                selected = 3;
+            }
+            else if (!reset)
+            {
+                pictureBox1.Image = global::LevelEditor.Properties.Resources.energizer;
+                selected = 0;
+                saveimage = global::LevelEditor.Properties.Resources.empty;
+            }
+            else if (selected == 3)
+                pictureBox1.Image = global::LevelEditor.Properties.Resources.energizer;
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if ((selected!= 4) && (!reset))
+            {
+                resetPictures(sender, e, 4);
+                saveimage = pictureBox2.Image;
+                pictureBox2.Image = global::LevelEditor.Properties.Resources.Kamikazebrillance;
+                selected = 4;
+            }
+            else if (!reset)
+            {
+                pictureBox2.Image = global::LevelEditor.Properties.Resources.Kamikaze;
+                selected = 0;
+                saveimage = global::LevelEditor.Properties.Resources.empty;
+            }
+            else if (selected == 4)
+                pictureBox2.Image = global::LevelEditor.Properties.Resources.Kamikaze;
+        }
+
+        private void pictureBox6_Click_1(object sender, EventArgs e)
+        {
+            if ((selected!= 5) && (!reset))
+            {
+                resetPictures(sender, e, 5);
+                saveimage = pictureBox6.Image;
+                pictureBox6.Image = global::LevelEditor.Properties.Resources.emptybrillance;
+                selected = 5;
+            }
+            else if (!reset)
+            {
+                pictureBox6.Image = global::LevelEditor.Properties.Resources.empty;
+                selected = 0;
+                saveimage = global::LevelEditor.Properties.Resources.empty;
+            }
+            else if (selected == 5)
+                pictureBox6.Image = global::LevelEditor.Properties.Resources.empty;
+        }
+
+        private void pictureBox8_Click_1(object sender, EventArgs e)
+        {
+            string position = "1";
+            string time = Convert.ToString(page + 200);
+            Stream stream = new FileStream("level.xpa",FileMode.Open);
+            StreamWriter sw = new StreamWriter(stream);
+            StreamReader sr = new StreamReader(stream);
+            string vaisseau;
+            string text;
+
+            switch (selected)
+            {
+                case 1:
+                    vaisseau = "drone";
+                    break;
+                case 2:
+                    vaisseau = "doubleshooter";
+                    break;
+                case 3:
+                    vaisseau = "energizer";
+                    break;
+                case 4:
+                    vaisseau = "kamikaze";
+                    break;
+                default:
+                    vaisseau = "drone";
+                    break;
+            }
+
+            text = sr.ReadToEnd();
+            string[] textArray = text.Split('\n');
+
+            if (pictureBox8.Image != global::LevelEditor.Properties.Resources.empty) // on remplace une ancienne ligne
+            {
+                try
+                {
+                    textArray[Convert.ToInt16(position) + page * 10] = ("vaisseau" + ";" + vaisseau + ":" + position + " " + time);
+                }
+                catch
+                { }
+
+                for (int i = 0; i < textArray.Length; i++)
+                {
+                    sw.WriteLine(textArray[i]);
+                }
+            }
+            else // nouvelle ligne
+            {
+
+                if (selected != 0)
+                {
+                    sw.WriteLine("vaisseau" + ";" + vaisseau + ":" + position + " " + time);
+                }
+                else
+                    sw.WriteLine("");
+
+                emplacements.Add(Convert.ToInt16(position) + page * 10); //On ajoute 
+            }
+
+            sw.Close();
+            
+            
+            pictureBox8.Image = saveimage;
+        }
+        
+
+    }
+}
---
+++ LevelEditor/LevelEditor/Plot.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LevelEditor
+{
+    class Plot
+    {
+        string _position, _time;
+        int _selected,  _compteur;
+        int _page;
+
+        public Plot(string position, string time, int page, int compteur, int selected, System.Windows.Forms.PictureBox pictureBox, System.Drawing.Image saveimage)
+        {
+            Stream stream;
+            _position = position;
+            _time = time;
+            _selected = selected;
+            _page = page;
+            _compteur = compteur;
+            if(_compteur == 0&&_page==0&&_selected!=(-1)) //suppression de l'ancien fichier de travail
+                 stream = new FileStream("Work", FileMode.Create);
+            else
+                 stream = new FileStream("Work", FileMode.Open);
+            StreamWriter sw = new StreamWriter(stream);
+            StreamReader sr = new StreamReader(stream);
+            string vaisseau;
+            string text;
+                 switch (selected) // ICI AJOUTER LES NOUVEAUX VAISSEAUX/BONUS
+                {
+                    case 1:
+                        vaisseau = "drone";
+                        break;
+                    case 2:
+                        vaisseau = "rapidshooter";
+                        break;
+                    case 3:
+                        vaisseau = "blasterer";
+                        break;
+                    case 4:
+                        vaisseau = "kamikaze";
+                        break;
+                    case 5:
+                        vaisseau = "vie";
+                        break;
+                    case 6:
+                        vaisseau = "baseWeapon";
+                        break;
+                    case 7:
+                        vaisseau = "vie";
+                        break;
+                    default:
+                        vaisseau = "drone";
+                        break;
+                }
+                if (_selected >0)
+                {
+                    if (page == 0)
+                    {
+                        if (_compteur == 0)
+                        {
+                            for (int i = 0; i < Convert.ToInt16(_position); i++)
+                                sw.Write("\n");
+
+                            for (int i = Convert.ToInt16(_position); i < 72; i++)
+                            {
+                                if (i == 71)
+                                    sw.Write("");
+                                else
+                                    sw.Write("\n");
+                            }
+
+                            _compteur++;
+                        }
+                    }
+                    else // on démarre de la fin du fichier pour une nouvelle page
+                    {
+                        string texttest = sr.ReadToEnd();
+                        string[] texttestArray = texttest.Split('\n');
+                        if (texttestArray.Length < 72 * (page + 1))
+                        {
+                            if (_compteur == 0)
+                            {
+                                for (int i = 0; i < Convert.ToInt16(_position); i++)
+                                    sw.Write("\n");
+
+                                for (int i = Convert.ToInt16(_position); i < 72; i++)
+                                {
+                                    if (i == 73)
+                                        sw.Write("");
+                                    else
+                                        sw.Write("\n");
+                                }
+
+                                _compteur++;
+                            }
+                        }
+                    }
+                }
+                sw.Close();
+                stream.Close();
+                Stream stream_int = new FileStream("Work", FileMode.Open);
+                StreamReader sr_int = new StreamReader(stream_int);
+
+                text = sr_int.ReadToEnd();
+                string[] textArray = text.Split('\n');
+                sr_int.Close();
+                stream_int.Close();
+
+                Stream stream_new = new FileStream("Work", FileMode.Open);
+                StreamReader sr_new = new StreamReader(stream_new);
+
+                string type = "";
+
+                if ((selected == 6) || (selected == 7))
+                    type = "bonus";
+                else
+                    type = "vaisseau";
+
+                if (selected == 0)
+                    textArray[Convert.ToInt16(_position) + page * 72 - 1 + 9 * ((Convert.ToInt32(_time) - 400) / ((400 + 3200 * page)))] = ("                                                                                                                       ");// epic bug de buffer confirmed by nurelin, xavier, et lecuyer, don't try to guess why.
+                else if ((selected < 10)&&(selected >0))
+                    textArray[Convert.ToInt16(_position) + page * 72 - 1 + 9 * ((Convert.ToInt32(_time) - 400) / ((400 + 3200*page)))] = (type + ";" + vaisseau + ":" + position + " " + time);
+
+                StreamWriter sw_new = new StreamWriter(stream_new);
+                for (int i = 0; i < textArray.Length; i++)
+                {
+                    if (i == textArray.Length - 1)
+                        sw_new.Write(textArray[i]);
+                    else
+                        sw_new.Write(textArray[i] + '\n');
+                }
+                sw_new.Close();
+                stream_new.Close();
+
+                pictureBox.Image = saveimage;
+            
+        }
+
+        public void Load(List<System.Windows.Forms.PictureBox> boxList, int page) 
+        {
+            List<System.Drawing.Image> listImageToLoad = new List<System.Drawing.Image>();
+            Stream stream = new FileStream("Work", FileMode.Open);
+            StreamReader sr = new StreamReader(stream);
+            string text = sr.ReadToEnd();
+            string[] textArray = text.Split('\n');
+
+                for (int i = 72 * page; i < textArray.Length; i++) // ICI AJOUTER LES NOUVEAUX VAISSEAUX/BONUS
+                {
+                    if (textArray[i].Contains("drone"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.drone);
+                    else if (textArray[i].Contains("kamikaze"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.Kamikaze);
+                    else if (textArray[i].Contains("blasterer"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.energizer);
+                    else if (textArray[i].Contains("rapidshooter"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.doubleshooter);
+                    else if (textArray[i].Contains("Life"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.Life);
+                    else if (textArray[i].Contains("baseWeapon"))
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.DoubleBaseWeapon);
+                    else
+                        listImageToLoad.Add(global::LevelEditor.Properties.Resources.Sans_titre);
+                }
+
+                foreach (System.Windows.Forms.PictureBox box in boxList)
+                {
+                    box.Image = listImageToLoad.First();
+                    listImageToLoad.RemoveAt(0);
+                }
+                sr.Close();
+                stream.Close();
+        }
+        
+        internal int New_compteur()
+        {
+            return (_compteur);
+        }
+
+
+    }
+}
